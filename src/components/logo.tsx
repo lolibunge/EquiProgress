3364b497e9370ef41dfeb2b7e7c734ae1eacb3c4
Dolores Bunge <loli@rebltech.com>
2025-08-15 02:00:17 +0000

    not quite there

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,8 +11,8 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20Z" />
-    <path d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" />
-    <path d="M12 12a5 5 0 1 1 0 10 5 5 0 0 1 0-10Z" />
+    <path d="M3.5 10C5.5 6.5 9.5 4 14 4C19.5 4 22 8 22 12C22 18 18 22 12 22C6 22 2 18 2 12C2 11 2.2 10 2.5 9" />
+    <path d="M14 4C13.5 6 13 8.5 12.5 11C12 13.5 11.5 15.5 11 17" />
+    <path d="M9.5 15c-1-1-1.5-2.5-1-4C9 9 10 7.5 11.5 6.5" />
   </svg>
 );
